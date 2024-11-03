749ed8cf6ae0253276f173262a2f50e1905245b9
janst.geo <janst.geo@gmail.com>
2024-11-03 01:35:31 +0000

    reinstalling npm packages that were missing through the merge

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
-import React from 'react';
-import NetworkGraph from './pages/NetworkGraph';
-import ChatPage from './pages/ChatPage';
+import './App.css'
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
+import {Button} from "@/components/ui/button.tsx";
 
-const App: React.FC = () => {
+function App() {
+
   return (
-    <div className="App">
-      {/* <h1>SkillSync</h1> */}
-      {/* <NetworkGraph /> */}
-      <ChatPage />
-    </div>
-  );
-};
+    <>
+      <header className='border-b bg-background'>
+        <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'>
+          <div className='flex h-16 items-center justify-between'>
+            <h2>SkillSync</h2>
+              <SignedOut>
+                <Button variant='default'>
+                  <SignInButton />
+                </Button>
+              </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </div>
+        </div>
+      </header>
+    </>
+  )
+}
 
-export default App;
+export default App
